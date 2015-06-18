51385fc3f984a6eb4bacbba9ab3c32e500c18ce9
John Poplett <john.poplett@acm.org>
2015-06-18 07:46:57 -0500

    Minor evtype cleanup.
    Signed-off-by: John Poplett <john.poplett@acm.org>

--- StormReport.R
+++ StormReport.R
@@ -11,3 +11,16 @@ storm.data.summary$INCIDENTS <- storm.data.summary$FATALITIES + storm.data.summa
 
 head(storm.data.summary, n = 10)
 head(storm.data.summary %>% arrange(desc(INCIDENTS)), n = 10)
+
+storm.data$EVTYPE <- as.character(storm.data$EVTYPE)
+storm.data$EVTYPE <- toupper(storm.data$EVTYPE)
+storm.data$EVTYPE <- gsub("\\s+", " ", storm.data$EVTYPE, perl = TRUE)
+storm.data$EVTYPE <- sub("^\\s+", "", storm.data$EVTYPE, perl = TRUE)
+storm.data$EVTYPE <- sub("\\s+$", "", storm.data$EVTYPE, perl = TRUE)
+storm.data$EVTYPE <- sub("\\s+$", "", storm.data$EVTYPE, perl = TRUE)
+storm.data$EVTYPE <- sub("HIGH SURF.*", "HIGH SURF", storm.data$EVTYPE, perl = TRUE)
+storm.data$EVTYPE <- sub("TSTM", "THUNDERSTORM", storm.data$EVTYPE, perl = TRUE)
+storm.data$EVTYPE <- sub("^THUNDERSTORM.*", "THUNDERSTORM", storm.data$EVTYPE, perl = TRUE)
+storm.data <- storm.data[!grepl("SUMMARY.*", storm.data$EVTYPE),]
+storm.data$EVTYPE <- as.factor(storm.data$EVTYPE)
+
