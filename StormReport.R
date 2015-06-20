efc1fea249383ba6c0b0be2175d308d2a384d9c9
John Poplett <john.poplett@acm.org>
2015-06-19 23:51:36 -0500

    Complete plot for human harm.
    Signed-off-by: John Poplett <john.poplett@acm.org>

--- StormReport.R
+++ StormReport.R
@@ -21,50 +21,52 @@ storm.data$EVTYPE <- gsub("\\s+", " ", storm.data$EVTYPE, perl = TRUE)
 storm.data$EVTYPE <- sub("^\\s+", "", storm.data$EVTYPE, perl = TRUE)
 storm.data$EVTYPE <- sub("\\s+$", "", storm.data$EVTYPE, perl = TRUE)
 storm.data$EVTYPE <- sub("\\s+$", "", storm.data$EVTYPE, perl = TRUE)
-storm.data$EVTYPE <- sub("(?:HEAVY|HIGH) SURF.*", "HIGH SURF", storm.data$EVTYPE, perl = TRUE)
-storm.data$EVTYPE <- sub("TSTM", "THUNDERSTORM", storm.data$EVTYPE, perl = TRUE)
+#storm.data$EVTYPE <- sub("(?:HEAVY|HIGH) SURF.*", "HIGH SURF", storm.data$EVTYPE, perl = TRUE)
+#storm.data$EVTYPE <- sub("TSTM", "THUNDERSTORM", storm.data$EVTYPE, perl = TRUE)
 
 
 #
 # Handle some synonyms
 #
-storm.data$EVTYPE <- sub("SHOWERS|PRECIPITATION", "RAIN", storm.data$EVTYPE, perl = TRUE)
-storm.data$EVTYPE <- sub("EXCESSIVE|RECORD", "HEAVY", storm.data$EVTYPE, perl = TRUE)
-storm.data$EVTYPE <- sub("DRIEST MONTH|DRY PATTERN|HEAVY DRYNESS|HEAVYLY DRY", "DROUGHT", storm.data$EVTYPE, perl = TRUE)
-storm.data$EVTYPE <- sub("TORRENTIAL RAINFALL", "HEAVY RAIN", storm.data$EVTYPE, perl = TRUE)
+# storm.data$EVTYPE <- sub("SHOWERS|PRECIPITATION", "RAIN", storm.data$EVTYPE, perl = TRUE)
+# storm.data$EVTYPE <- sub("EXCESSIVE|RECORD", "HEAVY", storm.data$EVTYPE, perl = TRUE)
+# storm.data$EVTYPE <- sub("DRIEST MONTH|DRY PATTERN|HEAVY DRYNESS|HEAVYLY DRY", "DROUGHT", storm.data$EVTYPE, perl = TRUE)
+# storm.data$EVTYPE <- sub("TORRENTIAL RAINFALL", "HEAVY RAIN", storm.data$EVTYPE, perl = TRUE)
 
 #
 # Special case occassions where "HEAVY" is used instead of "HIGH" and vice-versa.
 #
-storm.data$EVTYPE <- sub("HEAVY (SURF|WIND)", "HIGH \\1", storm.data$EVTYPE, perl = TRUE)
-storm.data$EVTYPE <- sub("HIGH (RAIN|SNOW)", "HEAVY \\1", storm.data$EVTYPE, perl = TRUE)
+# storm.data$EVTYPE <- sub("HEAVY (SURF|WIND)", "HIGH \\1", storm.data$EVTYPE, perl = TRUE)
+# storm.data$EVTYPE <- sub("HIGH (RAIN|SNOW)", "HEAVY \\1", storm.data$EVTYPE, perl = TRUE)
 
 #
 # Special case to handle evtypes that can have a "MARINE" prefix / qualifier
 #
-storm.data$EVTYPE <- sub("(?:(MARINE\\s+)|(.*))HAIL.*", "\\1HAIL", storm.data$EVTYPE, perl = TRUE)
-storm.data$EVTYPE <- sub("(?:(MARINE\\s+)|(.*))HIGH WIND.*", "\\1HIGH WIND", storm.data$EVTYPE, perl = TRUE)
-storm.data$EVTYPE <- sub("(?:(MARINE\\s+)|(.*))STRONG WIND.*", "\\1STRONG WIND", storm.data$EVTYPE, perl = TRUE)
-storm.data$EVTYPE <- sub("(?:(MARINE\\s+)|(.*))THUNDE.*[ST].*M.*", "\\1THUNDERSTORM WIND", storm.data$EVTYPE, perl = TRUE)
-
-storm.data$EVTYPE <- sub(".*HURRICANE.*", "HURRICANE (TYPHOON)", storm.data$EVTYPE, perl = TRUE)
-storm.data$EVTYPE <- sub(".*(HEAVY RAIN|RAIN \\(HEAVY\\)).*", "HEAVY RAIN", storm.data$EVTYPE, perl = TRUE)
-
-storm.data <- storm.data[!grepl("SUMMARY.*", storm.data$EVTYPE),]
+# storm.data$EVTYPE <- sub("(?:(MARINE\\s+)|(.*))HAIL.*", "\\1HAIL", storm.data$EVTYPE, perl = TRUE)
+# storm.data$EVTYPE <- sub("(?:(MARINE\\s+)|(.*))HIGH WIND.*", "\\1HIGH WIND", storm.data$EVTYPE, perl = TRUE)
+# storm.data$EVTYPE <- sub("(?:(MARINE\\s+)|(.*))STRONG WIND.*", "\\1STRONG WIND", storm.data$EVTYPE, perl = TRUE)
+# storm.data$EVTYPE <- sub("(?:(MARINE\\s+)|(.*))THUNDE.*[ST].*M.*", "\\1THUNDERSTORM WIND", storm.data$EVTYPE, perl = TRUE)
+# 
+# storm.data$EVTYPE <- sub(".*HURRICANE.*", "HURRICANE (TYPHOON)", storm.data$EVTYPE, perl = TRUE)
+# storm.data$EVTYPE <- sub(".*(HEAVY RAIN|RAIN \\(HEAVY\\)).*", "HEAVY RAIN", storm.data$EVTYPE, perl = TRUE)
+# 
+# storm.data <- storm.data[!grepl("SUMMARY.*", storm.data$EVTYPE),]
 
 storm.data$EVTYPE <- as.factor(storm.data$EVTYPE)
 
-evtypes.short.list <- evtypes$evtype[!evtypes$evtype %in% c("HAIL", "HIGH WIND", "STRONG WIND", "THUNDERSTORM WIND", "MARINE HAIL", "MARINE HIGH WIND", "MARINE STRONG WIND", "MARINE THUNDERSTORM WIND")]
+# evtypes.short.list <- evtypes$evtype[!evtypes$evtype %in% c("HAIL", "HIGH WIND", "STRONG WIND", "THUNDERSTORM WIND", "MARINE HAIL", "MARINE HIGH WIND", "MARINE STRONG WIND", "MARINE THUNDERSTORM WIND")]
+# 
+# for(evtype in evtypes.short.list) {
+#   storm.data$EVTYPE <- sub(sprintf(".*%s.*", evtype), evtype, storm.data$EVTYPE, perl = TRUE)
+# }
 
-for(evtype in evtypes.short.list) {
-  storm.data$EVTYPE <- sub(sprintf(".*%s.*", evtype), evtype, storm.data$EVTYPE, perl = TRUE)
-}
-
-unique(storm.data$EVTYPE)
+evtype.values.before <- length(unique(storm.data$EVTYPE))
+storm.data.rows.before <- nrow(storm.data)
 
 storm.data <- storm.data %>% filter(storm.data$EVTYPE %in% evtypes$evtype)
 
-unique(storm.data$EVTYPE)
+evtype.values.after <- length(unique(storm.data$EVTYPE))
+storm.data.rows.after <- nrow(storm.data)
 
 storm.data.summary <- storm.data %>%
   group_by(EVTYPE) %>%
@@ -73,8 +75,39 @@ storm.data.summary <- storm.data %>%
 
 storm.data.summary$INCIDENTS <- storm.data.summary$FATALITIES + storm.data.summary$INJURIES
 
-head(storm.data.summary, n = 10)
-head(storm.data.summary %>% arrange(desc(INCIDENTS)), n = 10)
+fatalities <- head(storm.data.summary, n = 5)
+injuries <- head(storm.data.summary %>% arrange(desc(INJURIES)), n = 5)
+incidents <- head(storm.data.summary %>% arrange(desc(INCIDENTS)), n = 5)
+
+fatalities <- fatalities %>% melt(id.vars = "EVTYPE", measure.vars = "FATALITIES", variable.name = "Category")
+injuries <- injuries %>% melt(id.vars = "EVTYPE", measure.vars = "INJURIES", variable.name = "Category")
+incidents <- incidents %>% melt(id.vars = "EVTYPE", measure.vars = "INCIDENTS", variable.name = "Category")
+
+most.harmful <- rbind(fatalities, injuries)
+
+ggplot(data=most.harmful, aes(x=EVTYPE, y=value, fill=Category)) +
+  geom_bar(stat="identity") +
+  labs(variable = "Foo") +
+  xlab("Storm Event") + ylab("Count") + # Set axis labels
+  ggtitle("Human Harm")
+
+ggplot(data=most.harmful, aes(x=EVTYPE, y=value, fill=Category)) + 
+  geom_bar(colour="black", stat="identity",
+           position=position_dodge(),
+           size=.3) +                        # Thinner lines
+  scale_fill_hue(name="Metric") +      # Set legend title
+  xlab("Storm Event") + ylab("Count") + # Set axis labels
+  ggtitle("Human Harm") +     # Set title
+  theme_bw()
+
+ggplot(data=most.harmful, aes(x=EVTYPE, y=value, fill=EVTYPE)) + 
+  geom_bar(colour="black", stat="identity",
+           position=position_dodge(),
+           size=.3) +                        # Thinner lines
+  xlab("Storm Event") + ylab("Count") + # Set axis labels
+  ggtitle("Human Harm") +     # Set title
+  theme_bw() + facet_grid(variable ~ .)
+
 
 how.many.has.text <- function(text) {
   unique(storm.data[grepl(text, storm.data$EVTYPE), "EVTYPE"])
